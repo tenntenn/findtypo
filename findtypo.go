f6bd466d51d31717c9822a2c99b46b4a1c011c57
Takuya Ueda <uedatakuya@gmail.com>
2017-02-14 10:39:36 +0900

    パッケージをインポートして.goファイルをパースする

--- findtypo.go
+++ findtypo.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"go/ast"
+	"go/build"
+	"go/parser"
 	"go/token"
 	"os"
+	"strings"
 )
+
+var (
+	ctx *build.Context
+)
+
+func init() {
+	c := build.Default // copy
+	ctx = &c
+	ctx.CgoEnabled = false
+	ctx.GOPATH = ""
+}
 
 type Typo struct {
 	Pos  token.Position
@@ -29,5 +45,59 @@ func FindTypo(paths []string) ([]*Typo, error) {
 }
 
 func findTypo(dir string, path string) ([]*Typo, error) {
+	pkg, err := ctx.Import(path, dir, build.IgnoreVendor)
+	if err != nil {
+		return nil, err
+	}
+
+	fset := token.NewFileSet()
+	pkgs, err := parser.ParseDir(fset, pkg.Dir, makeFilter(pkg), parser.ParseComments)
+	if err != nil {
+		return nil, err
+	}
+
+	var typos []*Typo
+	for _, p := range pkgs {
+		for _, f := range p.Files {
+			t, err := findTypoByFile(fset, f)
+			if err != nil {
+				return nil, err
+			}
+			typos = append(typos, t...)
+		}
+	}
+
+	return typos, nil
+}
+
+// 不要なファイルを省く
+func makeFilter(pkg *build.Package) func(fi os.FileInfo) bool {
+	return func(fi os.FileInfo) bool {
+		// テストファイルを省く
+		if strings.HasSuffix(fi.Name(), "_test.go") {
+			return false
+		}
+
+		// 無視されているファイルを省く
+		for _, ignored := range pkg.IgnoredGoFiles {
+			if ignored == fi.Name() {
+				return false
+			}
+		}
+
+		// CGO関係のファイルを省く
+		for _, cgofile := range pkg.CgoFiles {
+			if cgofile == fi.Name() {
+				return false
+			}
+		}
+
+		return true
+	}
+}
+
+// ファイルごとにタイポを見つける
+func findTypoByFile(fset *token.FileSet, f *ast.File) ([]*Typo, error) {
+	fmt.Println(fset.Position(f.Pos()).Filename)
 	return nil, nil
 }
